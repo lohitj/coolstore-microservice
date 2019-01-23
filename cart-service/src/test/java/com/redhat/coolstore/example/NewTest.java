4aec783efc58e8379a866dcbf6781d33fb92e42e
lohitj <39816251+lohitj@users.noreply.github.com>
2019-01-23 12:49:29 +0530

    Update NewTest.java

--- cart-service/src/test/java/com/redhat/coolstore/example/NewTest.java
+++ cart-service/src/test/java/com/redhat/coolstore/example/NewTest.java
@@ -1,4 +1,5 @@
-pacakge example;
+package com.redhat.coolstore;
+package example;
 import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;		
 import org.openqa.selenium.firefox.FirefoxDriver;		
