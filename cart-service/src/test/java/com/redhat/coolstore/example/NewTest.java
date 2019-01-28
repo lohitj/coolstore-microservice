b73bdc5cc6e9891b71ea6a754f223872e7a4ffb8
lohitj <39816251+lohitj@users.noreply.github.com>
2019-01-28 11:40:05 +0530

    Update NewTest.java

--- cart-service/src/test/java/com/redhat/coolstore/example/NewTest.java
+++ cart-service/src/test/java/com/redhat/coolstore/example/NewTest.java
@@ -7,6 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;	
 import org.testng.annotations.BeforeTest;	
 import org.testng.annotations.AfterTest;
+import org.openqa.selenium.MutableCapabilities
 public class NewTest {
  
     WebDriver driver;
