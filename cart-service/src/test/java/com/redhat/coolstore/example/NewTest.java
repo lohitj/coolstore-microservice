f407f87010009a3d16f9ea3e2a84dfb2c16d3b8f
lohitj <39816251+lohitj@users.noreply.github.com>
2019-01-28 12:13:01 +0530

    Update NewTest.java

--- cart-service/src/test/java/com/redhat/coolstore/example/NewTest.java
+++ cart-service/src/test/java/com/redhat/coolstore/example/NewTest.java
@@ -18,14 +18,14 @@ public class NewTest {
         String webDriverValue = System.getProperty("user.dir") +
                 "/target/tmp_webdrivers/chromedriver-linux-64bit";
         System.setProperty(webDriverKey, webDriverValue);
-        driver = new ChromeDriver();
-        driver.get("http://www.google.com");
+//         driver = new ChromeDriver();
+//         driver.get("http://www.google.com");
     }
  
 //todo: test
  
     @AfterTest
     public void afterTest() {
-        driver.quit();
+//         driver.quit();
     }	
 }
