006f0d58c9e04274acb8eea709e041de543873c5
lohitj <39816251+lohitj@users.noreply.github.com>
2019-02-21 22:11:57 +0530

    Update NewIT.java

--- cart-service/src/test/java/com/redhat/coolstore/example/NewIT.java
+++ cart-service/src/test/java/com/redhat/coolstore/example/NewIT.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.Assert;
-	import org.junit.Test;
+import org.junit.Test;
 		
 
 import org.openqa.selenium.MutableCapabilities;
